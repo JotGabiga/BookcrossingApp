d057501ca910dcf3a7b6ce518147873c0a943fae
Justyna Gabiga <jot.gabiga@gmail.com>
2021-10-10 20:35:44 +0200

    index file modified

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -62,7 +62,7 @@ const Home = (props) => {
             </section>
             <section className="searchSection">
                 <div className="textSection">
-                    <h1>Wymień się książką!</h1>
+                    <h1>Wymień się książką! Test test test</h1>
                     <article>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt est in laoreet tincidunt. Morbi facilisis nisi eu feugiat lacinia. Proin ornare eu purus id porttitor. Donec et mattis libero. Aliquam erat volutpat. Fusce tempus nisi at nisi efficitur, vitae malesuada dui euismod. Phasellus auctor id sapien id malesuada. 
                     </article>
