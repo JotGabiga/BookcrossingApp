367215dcff0eec39c951ce4febb37b65e7a0b242
Justyna Gabiga <jot.gabiga@gmail.com>
2021-10-15 13:56:21 +0200

    Rating bug fixed

--- src/components/RatingStar/RatingStar.jsx
+++ src/components/RatingStar/RatingStar.jsx
@@ -6,7 +6,7 @@ const RatingStar = (props) => {
     return (
         <section className="ratingStar">
            <img src={ratingStar} alt="rating star"></img>
-           <text>{props.ratingProps.rating}</text>
+           <text>{props.ratingProps}</text>
         </section>
   );
 };
