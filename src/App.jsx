a39b53f4f0558fed36a7b06c90456426ef6827fe
Justyna Gabiga <jot.gabiga@gmail.com>
2022-01-13 14:09:55 +0100

    IE problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,18 @@ import Settings from "./components/Settings/Settings";
 import BookPage from "./components/BookPage/BookPage";
 import Start from "./components/Start/Start";
 import { Fragment } from "react";
+import { isIE } from 'react-device-detect';
+import IeProblem from './ieProblem.gif';
+
 function App() {
+  if (isIE)
+  return (
+    <div className="ieMessage">
+      <img src={IeProblem} alt="Sad face"></img>
+      <h1>Hi there. You’re using an outdated browser</h1>
+      <p>For a safer and faster user experience use a modern browser like Chrome or Edge.</p>
+    </div>
+  )
   return (
     <Switch>
       <Fragment>
